dfe1fcbe8b159a7cffdf5c3f20a34ef2d8486f5a
Nallieheai <21331007+Nallieheai@users.noreply.github.com>
2024-11-19 17:17:13 +0100

    Can now fetch all users from the clan

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-const { WOMClient } = require('@wise-old-man/utils');
+// const { WOMClient } = require('@wise-old-man/utils');
+import { WOMClient } from "@wise-old-man/utils";
 
-// Base URL: https://api.wiseoldman.net/v2
 
-console.log("Hello World!");
+/*
+ * WOM Base URL: https://api.wiseoldman.net/v2
+ * CollectionLog Base URL: https://api.collectionlog.net/
+ * Iron Relax groupId: 5657
+ */
+
+const groupId = 5657;
+const baseUrl = "https://api.wiseoldman.net/v2";
+
+const client = new WOMClient();
+
+fetch(`${baseUrl}/groups/${groupId}`).then(res => res.json()).then(data => {
+    for (let i = 0; i < data.memberships.length; i++) {
+        console.log("Member: ", data.memberships[i].player.displayName)
+    }
+}).catch(err => console.error(err));
+
+// client.players.updatePlayer("Nallieheai");
+// client.groups.getRequest(`${baseUrl}/groups/${groupId}`).then(data => console.log(data));
