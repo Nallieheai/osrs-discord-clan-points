0d1200043f6482d2db565628bf7a084396ab66a2
Nallieheai <21331007+Nallieheai@users.noreply.github.com>
2025-03-17 14:51:57 +0100

    Formatted the code & added parameters for future bonus points for 1st, 2nd, 3rd and nth place

--- src/services/IronRelax.ts
+++ src/services/IronRelax.ts
@@ -1,9 +1,14 @@
 import { ParticipationWithPlayerAndProgress } from "@wise-old-man/utils";
 import { formatNumber } from "../utils/Helpers";
 
-export async function getVariablePointsCompetition(participants: ParticipationWithPlayerAndProgress[], eligibleAmount: number, maxPoints: number) {
+export async function getVariablePointsCompetition(participants: ParticipationWithPlayerAndProgress[],
+    eligibleAmount: number, maxPoints: number, bonusAmount: number = 0, bonusCount: number = 0) {
     return participants.map(participant => {
         const points = Math.floor(participant.progress.gained / eligibleAmount);
-        return ({"user": participant.player.displayName, "points": points >= maxPoints ? maxPoints : points, "xp": formatNumber(participant.progress.gained)});
+        return ({
+            "user": participant.player.displayName,
+            "points": points >= maxPoints ? maxPoints : points,
+            "xp": formatNumber(participant.progress.gained)
+        });
     });
 }
